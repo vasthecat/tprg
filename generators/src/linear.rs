ad7ee2f930768249d915a5575e63310716043252
Andrew Guschin <guschin.drew@gmail.com>
2023-02-13 15:45:14 +0400

    Добавлен линейный генератор

--- generators/src/linear.rs
+++ generators/src/linear.rs
@@ -4,18 +4,19 @@ pub struct LinearPRG {
     m: u64,
     a: u64,
     c: u64,
-    x_0: u64,
+    x: u64,
 }
 
 impl LinearPRG {
     pub fn new(m: u64, a: u64, c: u64, x_0: u64) -> Self {
-        Self { m, a, c, x_0 }
+        Self { m, a, c, x: x_0 }
     }
 }
 
 impl PRGenerator for LinearPRG {
     fn next(&mut self) -> u64 {
-        self.m += 1;
-        return self.m;
+        let x = self.x;
+        self.x = (self.a * self.x + self.c) % self.m;
+        return x;
     }
 }
