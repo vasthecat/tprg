1250eee3419f820bbf9e77b936cedd1467318fca
Andrew Guschin <guschin.drew@gmail.com>
2023-06-17 12:56:22 +0400

    Добавлен парсер параметров для второго задания

---
+++ rnc/src/clp.rs
@@ -0,0 +1,174 @@
+#[derive(Debug)]
+pub enum DistributionType {
+    Standrard,   // стандартное равномерное с заданным интервалом;
+    Triangle,    // треугольное распределение;
+    Exponential, // общее экспоненциальное распределение;
+    Normal,      // нормальное распределение;
+    Gamma,       // гамма распределение;
+    Lognormal,   // логнормальное распределение;
+    Logistic,    // логистическое распределение;
+    Binomial,    // биномиальное распределение.
+}
+
+impl DistributionType {
+    fn parse(name: &str) -> Option<Self> {
+        match name {
+            "st" => Some(DistributionType::Standrard),
+            "tr" => Some(DistributionType::Triangle),
+            "ex" => Some(DistributionType::Exponential),
+            "nr" => Some(DistributionType::Normal),
+            "gm" => Some(DistributionType::Gamma),
+            "ln" => Some(DistributionType::Lognormal),
+            "ls" => Some(DistributionType::Logistic),
+            "bi" => Some(DistributionType::Binomial),
+            &_ => None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Config {
+    pub distribution: DistributionType,
+    pub file: String,
+    pub p1: u32,
+    pub p2: u32,
+    pub p3: Option<u32>,
+}
+
+fn parse_name(arg: &String) -> (String, String) {
+    let mut name = Vec::new();
+    let mut i = 0;
+
+    while !arg[i..].starts_with(':') {
+        name.push(arg.as_bytes()[i]);
+        i += 1;
+        if i >= arg.len() {
+            break;
+        }
+    }
+    let name = String::from_utf8(name).unwrap();
+    return (name, arg[i..].to_string());
+}
+
+const HELP_STR: &str =
+    "Использование: ./rnc /d:<код> /f:<имя_файла> /p1:<число> /p2:<число> [/p3:<число>] [/h]
+/d:<код_метода>       - параметр указывает на метод распределения ППСЧ
+  Допустимые значения:
+   * st – стандартное равномерное с заданным интервалом;
+   * tr – треугольное распределение;
+   * ex – общее экспоненциальное распределение;
+   * nr – нормальное распределение;
+   * gm – гамма распределение;
+   * ln – логнормальное распределение;
+   * ls – логистическое распределение;
+   * bi – биномиальное распределение.
+/f:<полное_имя_файла> - полное имя файла, из которого будет считываться последовательность.
+/p1:<число>           - первый параметр.
+/p2:<число>           - второй параметр.
+/p3:<число>           - третий параметр (для гамма-распределения).
+/h                    - вывод доступных параметров";
+
+pub fn parse_args(args_iter: std::env::Args) -> Result<Config, String> {
+    let mut distribution = None;
+    let mut file = String::from("rnd.dat");
+    let mut p1 = None;
+    let mut p2 = None;
+    let mut p3 = None;
+
+    for arg in args_iter {
+        if !arg.starts_with('/') {
+            continue;
+        }
+
+        let rest = arg[1..].to_string();
+        let (name, rest) = parse_name(&rest);
+
+        if name == "h" {
+            return Err(HELP_STR.to_string());
+        }
+
+        if !rest.starts_with(':') {
+            return Err(format!(
+                "Не указано значение параметра '{}'\n\n{}",
+                name, HELP_STR
+            ));
+        }
+
+        let value = rest[1..].to_string();
+        match name.as_str() {
+            "d" => match DistributionType::parse(&value) {
+                None => {
+                    return Err(format!(
+                        "Недопустимое значение кода распределения\n\n{}",
+                        HELP_STR
+                    ))
+                }
+                distrtype => distribution = distrtype,
+            },
+            "f" => file = value,
+            "p1" => match value.parse::<u32>() {
+                Ok(num) => p1 = Some(num),
+                Err(_) => {
+                    return Err("Значение аргумента p1 должно быть \
+                        неотрицательным числом"
+                        .to_string())
+                }
+            },
+            "p2" => match value.parse::<u32>() {
+                Ok(num) => p2 = Some(num),
+                Err(_) => {
+                    return Err("Значение аргумента p2 должно быть \
+                        неотрицательным числом"
+                        .to_string())
+                }
+            },
+            "p3" => match value.parse::<u32>() {
+                Ok(num) => p3 = Some(num),
+                Err(_) => {
+                    return Err("Значение аргумента p3 должно быть \
+                        неотрицательным числом"
+                        .to_string())
+                }
+            },
+            argname => {
+                return Err(format!(
+                    "Неизвестный параметр '{}'\n\n{}",
+                    argname, HELP_STR
+                ))
+            }
+        }
+    }
+
+    if distribution.is_none() {
+        return Err(format!(
+            "Параметр 'd' является обязательным\n\n{}",
+            HELP_STR
+        ));
+    }
+
+    if p1.is_none() {
+        return Err(format!(
+            "Параметр 'p1' является обязательным\n\n{}",
+            HELP_STR
+        ));
+    }
+
+    if p2.is_none() {
+        return Err(format!(
+            "Параметр 'p2' является обязательным\n\n{}",
+            HELP_STR
+        ));
+    }
+
+    if let (Some(distribution), Some(p1), Some(p2)) = (distribution, p1, p2) {
+        return Ok(Config {
+            distribution,
+            file,
+            p1,
+            p2,
+            p3,
+        });
+    }
+
+    return Err(HELP_STR.to_string());
+}
--- rnc/src/main.rs
+++ rnc/src/main.rs
@@ -1,3 +1,66 @@
+use std::fs::File;
+use std::io::Write;
+
+mod clp;
+use crate::clp::{parse_args, Config, DistributionType};
+
+mod prdistribution;
+use crate::prdistribution::PRDistribution;
+
 fn main() {
-    println!("Hello, world!");
+    match parse_args(std::env::args()) {
+        Ok(conf) => match construct_distribution(&conf) {
+            Ok(gen) => {
+                // let numbers = generate_numbers(&conf, gen);
+                // write_numbers(&conf, &numbers);
+            }
+            Err(msg) => println!("{}", msg),
+        },
+        Err(msg) => println!("{}", msg),
+    };
 }
+
+fn construct_distribution(
+    conf: &Config,
+) -> Result<Box<dyn PRDistribution>, String> {
+    match conf.distribution {
+        _ => todo!(),
+    }
+}
+
+fn read_numbers(conf: &Config) -> Vec<u32> {
+    Vec::new()
+}
+
+// fn generate_numbers<T: PRGenerator + ?Sized>(
+//     conf: &Config,
+//     mut gen: Box<T>,
+// ) -> Vec<u32> {
+//     let mut numbers = Vec::new();
+//     let mut pb = ProgressBar::new(conf.n);
+//     for _ in 0..conf.n {
+//         numbers.push(gen.next());
+//         pb.message("Генерация чисел: ");
+//         pb.inc();
+//     }
+//     pb.finish_println("Генерация чисел завершена.");
+//     println!();
+//     return numbers;
+// }
+
+// fn write_numbers(conf: &Config, numbers: &Vec<u32>) {
+//     let mut file = File::create(&conf.file).unwrap();
+//     let mut pb = ProgressBar::new(numbers.len().try_into().unwrap());
+//     for i in 0..numbers.len() {
+//         let num = numbers[i];
+//         if i == numbers.len() - 1 {
+//             let _ = file.write_all(format!("{}", num).as_bytes());
+//         } else {
+//             let _ = file.write_all(format!("{},", num).as_bytes());
+//         }
+//         pb.message("Запись чисел в файл: ");
+//         pb.inc();
+//     }
+//     pb.finish_println("Запись чисел завершена.");
+//     println!();
+// }
